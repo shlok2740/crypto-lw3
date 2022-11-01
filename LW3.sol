3d5509326d48970fdedf0376c3705a875f29c8c4
Shlok Kumar <shlokkumar2303@gmail.com>
2022-11-01 14:35:08 +0530

    finished learnweb3 ch9

---
+++ LW3.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract LW3Token is ERC20 {
+    constructor(string memory _name, string memory _symbol)
+        ERC20(_name, _symbol)
+    {
+        _mint(msg.sender, 1000 * 10**18);
+    }
+
+    function mint() public {
+        _mint(msg.sender, 1000 * 10**18);
+    }
+}
